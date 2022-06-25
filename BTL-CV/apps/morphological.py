3a4b3b29c2b3c44cf3babafbccd0b47930d704e2
0377581629 <quanghuy2152@gmail.com>
2022-06-25 23:34:24 +0700

    add morphological dilation

--- BTL-CV/apps/morphological.py
+++ BTL-CV/apps/morphological.py
@@ -7,34 +7,22 @@ import copy
 
 
 def app():
-    DEMO_IMAGE = 'imgTests/erosion.jpg'
+    demo_image = 'imgTests/erosion.jpg'
+    selected_box = st.sidebar.selectbox('Choose one of the operations', ('None', 'Erosion', 'Dilation'))
 
-    selected_box = st.sidebar.selectbox('Choose one of the filters', ('None', 'Erosion'))
+    # Begin upload img
 
     def load_image():
         img_file_buffer = st.file_uploader("Upload an image", type=["jpg", "jpeg", 'png'])
         if img_file_buffer is not None:
             image = np.array(Image.open(img_file_buffer))
         else:
-            demo_image = DEMO_IMAGE
             image = np.array(Image.open(demo_image))
 
         st.image(image, caption=f"Original Image", use_column_width=True)
         return image
 
-    def erosion(photo):
-        # Read Image
-        original_image = photo.copy()
-        gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-        img = cv2.cvtColor(gray_image, cv2.COLOR_BGR2RGB)
-
-        (thresh, blackAndWhiteImage) = cv2.threshold(
-            img, 127, 255, cv2.THRESH_BINARY)
-
-        kernel = np.ones((3, 3), np.uint8)
-        output_image = cv2.erode(blackAndWhiteImage, kernel, iterations=2)
-
-        return output_image
+    # End upload img
 
     if selected_box == 'None':
         st.title('Morphological')
@@ -45,14 +33,66 @@ def app():
         st.subheader("Available Filters", anchor=None)
 
         st.markdown(
-            '<ul> <li> Erosion <li> </ul>',
+            '<ul> <li> Erosion <li> <li> Dilation <li> </ul>',
             unsafe_allow_html=True)
 
+    # Begin Erosion
+
+    def erosion(original_image,level):
+        # Read Image
+        gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(gray_image, cv2.COLOR_BGR2RGB)
+
+        (thresh, blackAndWhiteImage) = cv2.threshold(
+            img, 127, 255, cv2.THRESH_BINARY)
+
+        kernel = np.ones((3, 3), np.uint8)
+        output_image = cv2.erode(blackAndWhiteImage, kernel, iterations=level)
+
+        return output_image
+
     if selected_box == 'Erosion':
+
+        selected_box_operation_level = st.sidebar.selectbox('Choose one of the level', (1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
         st.title('Erosion Morphological')
         image = load_image()
         useWH = st.button('CONVERT')
 
         if useWH:
-            resized_image = erosion(image)
+            resized_image = erosion(image,selected_box_operation_level)
             st.image(resized_image, caption=f"Image with Erosion", use_column_width=True)
+
+    # End Erosion
+
+    # Begin Dilation
+
+    def dilation(original_image,level):
+        # Read Image
+        gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(gray_image, cv2.COLOR_BGR2RGB)
+
+        (thresh, blackAndWhiteImage) = cv2.threshold(
+            img, 127, 255, cv2.THRESH_BINARY)
+
+        kernel = np.ones((3, 3), np.uint8)
+        output_image = cv2.dilate(blackAndWhiteImage, kernel, iterations=level)
+
+        return output_image
+
+    if selected_box == 'Dilation':
+
+        selected_box_operation_level = st.sidebar.selectbox('Choose one of the level', (1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+        st.title('Dilation Morphological')
+        image = load_image()
+        useWH = st.button('CONVERT')
+
+        if useWH:
+            resized_image = dilation(image,selected_box_operation_level)
+            st.image(resized_image, caption=f"Image with Dilation", use_column_width=True)
+
+    # End Dilation
+
+
+
